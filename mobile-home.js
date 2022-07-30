39b54cd7f4bdf21d078bc9bc84c05556eb98552d
Julian <105754345+jprune@users.noreply.github.com>
2022-07-30 17:41:54 +0200

    todo button func

--- mobile-home.js
+++ mobile-home.js
@@ -41,10 +41,12 @@ const render = (array) => {
   array.forEach(element => {
       const todo = document.createElement("div");
       todo.classList.add("todo-render");
+        const todoBtns = document.createElement("div")
+        todoBtns.classList.add("btn-todo")
 
-        const todoCheck = document.createElement("input");
+        const todoCheck = document.createElement("button");
         todoCheck.classList.add("check-todo")
-        todoCheck.setAttribute("type", "checkbox")
+        todoCheck.innerHTML = `<img src="./assets/done.svg" alt="check-button">`;
         //!! Instead of input field work with label to customize checkbox experience?!!
 
         const todoTitle = document.createElement("h3");
@@ -56,15 +58,25 @@ const render = (array) => {
         const delBtn = document.createElement("button");
         delBtn.classList.add("delete-todo");
         delBtn.setAttribute(`id`, `${element.id}`)
-        delBtn.innerText = "DELETE";
+        delBtn.innerHTML = `<img src="./assets/delete.svg" alt="delete-button">`;
         /*  delBtn.addEventListener("click", function () {
         console.log(`Deleting ${myToDosHeadings[i]}`);
         }); */
-      todo.appendChild(todoCheck);
+
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("editBtn-todo");
+        editBtn.setAttribute(`id`, `${element.id}`)
+        editBtn.innerHTML = `<img src="./assets/edit.svg" alt="edit-button">`;
+        
+      todoBtns.appendChild(todoCheck);
+      todoBtns.appendChild(editBtn)
+      todoBtns.appendChild(delBtn);
+      todo.appendChild(todoBtns);
       todo.appendChild(todoTitle);
       todo.appendChild(desc);
-      todo.appendChild(delBtn);
+      
       renderTaskContainer.appendChild(todo); 
+      toDoBtnFunctions();
   }); 
 }
 
@@ -78,13 +90,11 @@ function addTaskBtnListener(addTaskBtn) {
         return window.alert("please input value")
     } else {
         let id = myToDos.length + 1;
-        console.log(myToDos)
-        myToDos.push({
+        myToDos.unshift({
             title: inputHeadingEl.value, 
             description: inputDescriptionEl.value, 
             id: id
         });
-        console.log(myToDos)
         inputHeadingEl.value = '';
         inputDescriptionEl.value = '';
         localStorage.setItem("myToDos", JSON.stringify(myToDos))
@@ -100,4 +110,33 @@ function cancelBtnListener(cancelTaskBtn) {
     e.preventDefault();
     addTodoDiv.innerHTML = "";
   })
+}
+
+function toDoBtnFunctions() {
+  const renderTaskContainer = document.querySelector('.renderTasks');
+  renderTaskContainer.addEventListener("click", function (e) {
+    //if a button with the class "delete-todo" is clicked
+    console.log(e)
+
+    if (e.target.parentNode.classList.contains("delete-todo")) {
+        //only return items of the array which are NOT equal to the id 
+        myToDos = myToDos.filter(el => el.id !== parseInt(e.target.parentNode.id))
+        render(myToDos)
+    }
+  
+    //cross h3, p when checkbox is clicked
+    if (e.target.parentNode.classList.contains("check-todo")) {
+      if (e.target.parentNode.parentNode.parentNode.style.textDecoration === "") {
+        e.target.parentNode.parentNode.parentNode.style.textDecoration = "line-through"  
+      } else {
+        e.target.parentNode.parentNode.parentNode.style.textDecoration = ""  
+      }
+    }
+
+    if (e.target.parentNode.classList.contains("editBtn-todo")) {
+      //find position in array with id
+      //open editor with content inside
+      //either way overwrite edit or cancel func
+    }
+  });
 }
