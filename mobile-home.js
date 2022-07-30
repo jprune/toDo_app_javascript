be24a9d6f450a7bda2882b4a7d2fbb19fb52c104
Julian <105754345+jprune@users.noreply.github.com>
2022-07-30 16:07:45 +0200

    form element functionality finished

--- mobile-home.js
+++ mobile-home.js
@@ -1,7 +1,9 @@
+let myToDos = [];
 
 const dateEl = document.getElementById('dateToday')
 const addTodoDiv = document.getElementById('add-todo')
-const addBtn = document.getElementById('edit-todo')
+const createTodoBtn = document.getElementById('edit-todo')
+
 
 //set today's date
 const renderDate = () => {
@@ -20,10 +22,82 @@ const createInputForm = () => {
       <input type="text" id="input-heading" name="fname" placeholder="e.g. Walk your Dog">
       <input type="textarea" id="input-description" name="fname" placeholder="Description">
       <div class="buttons">
-          <button class="addTaskBtn">Add task</button>
+          <button class="addTaskBtn" type="submit">Add task</button>
           <button class="cancelTaskBtn">Cancel</button>
       </div>
     </form>`
     addTodoDiv.innerHTML = formDiv
+    const addTaskBtn = document.querySelector('.addTaskBtn')
+    addTaskBtnListener(addTaskBtn)
+    const cancelTaskBtn = document.querySelector('.cancelTaskBtn')
+    cancelBtnListener(cancelTaskBtn)
 }
-addBtn.addEventListener('click', createInputForm)
+createTodoBtn.addEventListener('click', createInputForm)
+
+//render Tasks under input fields
+const render = (array) => {
+  const renderTaskContainer = document.querySelector('.renderTasks');
+  renderTaskContainer.innerHTML = "";
+  array.forEach(element => {
+      const todo = document.createElement("div");
+      todo.classList.add("todo-render");
+
+        const todoCheck = document.createElement("input");
+        todoCheck.classList.add("check-todo")
+        todoCheck.setAttribute("type", "checkbox")
+        //!! Instead of input field work with label to customize checkbox experience?!!
+
+        const todoTitle = document.createElement("h3");
+        todoTitle.innerText = element.title;
+
+        const desc = document.createElement("p");
+        desc.innerText = element.description;
+
+        const delBtn = document.createElement("button");
+        delBtn.classList.add("delete-todo");
+        delBtn.setAttribute(`id`, `${element.id}`)
+        delBtn.innerText = "DELETE";
+        /*  delBtn.addEventListener("click", function () {
+        console.log(`Deleting ${myToDosHeadings[i]}`);
+        }); */
+      todo.appendChild(todoCheck);
+      todo.appendChild(todoTitle);
+      todo.appendChild(desc);
+      todo.appendChild(delBtn);
+      renderTaskContainer.appendChild(todo); 
+  }); 
+}
+
+// get values of input if full and call render function
+function addTaskBtnListener(addTaskBtn) {
+  addTaskBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const inputHeadingEl = document.getElementById('input-heading')
+    const inputDescriptionEl = document.getElementById('input-description')
+    if (inputHeadingEl.value === '') {
+        return window.alert("please input value")
+    } else {
+        let id = myToDos.length + 1;
+        console.log(myToDos)
+        myToDos.push({
+            title: inputHeadingEl.value, 
+            description: inputDescriptionEl.value, 
+            id: id
+        });
+        console.log(myToDos)
+        inputHeadingEl.value = '';
+        inputDescriptionEl.value = '';
+        localStorage.setItem("myToDos", JSON.stringify(myToDos))
+        render(myToDos)
+        addTodoDiv.innerHTML = "";
+    }
+  })
+}
+
+//Close editor when cancelBtn is pressed
+function cancelBtnListener(cancelTaskBtn) {
+  cancelTaskBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    addTodoDiv.innerHTML = "";
+  })
+}
